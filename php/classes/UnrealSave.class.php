7942da51a59d2e3e03407c6b6e4aa9ba5295126a
Clint <clinton@jarvis.im>
2022-06-21 02:55:50 -0600

    zlib functions updated and added as needed. Updated exception and function names

--- php/classes/UnrealSave.class.php
+++ php/classes/UnrealSave.class.php
@@ -34,7 +34,7 @@ class UnrealReader {
         $data = fread($this->stream,$byteLength-1);
         $term = bin2hex(fread($this->stream,1));
         if ($term != '00') {
-            throw new Exception("Invalid string terminator. expecting \\0");
+            throw new Exception("Invalid string terminator. expecting \\0 (Str: 00)");
         }
         
         return $data;
@@ -44,25 +44,83 @@ class UnrealReader {
         
         $skipped = 0;
         $data = "";
-        while ($data = bin2hex(fread($this->stream,1)) != '78') {
-            $skipped++;
+        while (true) {
+            $data = fread($this->stream,1);
+            if (bin2hex($data) == '78') {
+                break;
+            } else {
+                $skipped++;
+            }
         }
-        $compressionLevel = bin2hex(fread($this->stream,1));
+        $cmf = $this->parse_zlibCMF($data);
+        $flg = $this->parse_zlibCINFO(fread($this->stream,1));
+        return array_merge($cmf,$flg);
+    }
+
+    function parse_zlibCMF($byte) {
+        //$bin = hex2bin(ord($byte));
+        //$bin = str_pad($bin, 8, 0, STR_PAD_LEFT);
+        $data = Array(
+            "CINFO" => '',
+            "CM" => ''
+        );
+
+        for ($i=0; $i<4; $i++) {
+            $data['CINFO'] .= (ord($byte) & (1<<$i))>>$i;
+        }
+        for ($i=4; $i<8; $i++) {
+            $data['CM'] .= (ord($byte) & (1<<$i))>>$i;
+        }
         
-        return "78".$compressionLevel;
+        return $data;
+    }
+
+    function parse_zlibCINFO($byte) {
+        $flags = Array(
+            "FCHECK" => '',
+            "FDICT" => '',
+            "FLEVEL" => '',
+        );
+
+        for ($i=0; $i<5; $i++) {
+            $flags['FCHECK'] .= (ord($byte) & (1<<$i))>>$i;
+        }
+        for ($i=5; $i<6; $i++) {
+            $flags['FDICT'] .= (ord($byte) & (1<<$i))>>$i;
+        }
+        $flags['FDICT'] = boolval($flags['FDICT']);
+        for ($i=6; $i<8; $i++) {
+            $flags['FLEVEL'] .= (ord($byte) & (1<<$i))>>$i;
+        }
+        switch ($flags['FLEVEL']) {
+            case '00':
+                $flags['FLEVEL'] = "fastest";
+                break;
+            case '01':
+                $flags['FLEVEL'] = "fast";
+                break;
+            case '11':
+                $flags['FLEVEL'] = "maximum";
+                break;
+            default:
+                $flags['FLEVEL'] = "default";
+                break;
+        }
+        return $flags;
     }
 
     function get_zlibDictID() {
-
+        // TODO: Add dictionary parser for zlib
     }
 
-    function get_remaining() {
+    function get_remainingBytes() {
         $data ="";
         while(!feof($this->stream)) {
             $data .= fread($this->stream,4096);
         }
         return $data;
     }
+
     function get_chunk(int $numBytes) {
         $data = fread($this->stream,$numBytes);
         
